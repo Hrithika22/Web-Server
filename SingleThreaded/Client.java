808bb6bc3a4d22d9ddb92a8f3fca7b1f84fea51b
Hrithika Gowlikar <hrithikagowlikar22@gmail.com>
2024-04-17 09:09:17 +0530

    Client SingleThreaded

---
+++ SingleThreaded/Client.java
@@ -0,0 +1,36 @@
+package SingleThreaded;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.InetAddress;
+import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
+
+public class Client {
+    public void run() throws UnknownHostException, IOException {
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello from the client" + socket.getLocalSocketAddress());
+        String line = fromSocket.readLine();
+        toSocket.close();
+        fromSocket.close();
+        socket.close();
+        System.out.println("Response from the socket is:" + line);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            Client client = new Client();
+            client.run();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
