d80f9d12e77ce5227a97c49344cdacc9d17fc1c9
Hrithika Gowlikar <hrithikagowlikar22@gmail.com>
2024-04-17 12:17:48 +0530

    multithreaded server

--- MultiThreaded/Server.java
+++ MultiThreaded/Server.java
@@ -1,4 +1,52 @@
+import java.net.ServerSocket;
+import java.util.function.Consumer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Server {
+    public Consumer<Socket> getConsumer(){
+        // return new Consumer<Socket>(){
+        //     @Override
+        //    public void accept(Socket clientSocket){
+            // try{
+            //     PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream());
+            //     toClient.println("Hello from the server");
+            //     toClient.close();
+            //     clientSocket.close();
+            //   }catch(IOException ex){
+            //     ex.printStackTrace();
+            //   }
+        //     }
+        // };
+        return(clientSocket)->{
+            try{
+              PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream());
+              toClient.println("Hello from the server");
+              toClient.close();
+              clientSocket.close();
+            }catch(IOException ex){
+              ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int port  = 8010;
+        Server server = new Server();
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(10000);//10000 millisecond = 10sec
+            System.out.println("Server is Listening on port" +port);
+            while(true){
+                Socket acceptedSocket = serverSocket.accept();
+                Thread thread = new Thread(()->server.getConsumer());
+                thread.start();
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
 
 }
